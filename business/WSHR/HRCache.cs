e60ae492bc663b4ffb5ef1871b1e806f079b5a82
Groll_shit <crysisma@yandex.ru>
2020-02-10 13:52:02 +0300

    Шаблон для HR

---
+++ business/WSHR/HRCache.cs
@@ -0,0 +1,21 @@
+﻿using models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace business.WSHR
+{
+    class HRCache
+    {
+        List<Teacher> Teachers { get; set; }
+        List<Lead> Leads { get; set; }
+        List<Group> Groups { get; set; }
+        List<Course> Courses { get; set; }
+        List<Skills> Skills { get; set; }
+        List<History> Historys { get; set; }
+        List<HistoryGroup> HistoryGroups { get; set; }
+        List<Status> Statuses { get; set; }
+        List<SkillsLead> SkillsLeads { get; set; }
+        List<SkillsLead> SkillsLeads { get; set; }
+    }
+}
--- business/WSHR/HRManager.cs
+++ business/WSHR/HRManager.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace business.WorkSpaceHR
+namespace business.WSHR
 {
-    class HRCache
+    class HRManager
     {
+
     }
 }
