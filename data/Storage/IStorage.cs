526736459a65e6d66ba588921ec9584bf6ed0486
filin0wl <nastia-dogf@mail.ru>
2020-02-12 14:54:03 +0300

    Add IStorage

---
+++ data/Storage/IStorage.cs
@@ -0,0 +1,18 @@
+﻿using models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace data.Storage
+{
+    interface IStorage
+    {
+        IEnumerable<IEntity> GetAll(string Tkey, string TValue);
+        IEnumerable<IEntity> GetAll();
+        bool Update(IEntity obj);
+        bool Add(IEntity obj);
+        bool Delete(IEntity obj);
+
+
+    }
+}
