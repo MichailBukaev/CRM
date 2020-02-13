4925deb4e764423117eabcb8342f431fbc1b7f95
MichailBukaev <miha.bukaev@gmail.com>
2020-02-13 18:06:09 +0300

    Рабочее место админа

---
+++ business/WSAdmin/Models/HRBusinessModelAdmin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace business.Models
+{
+    public class HRBusinessModel
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string SName { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ business/WSAdmin/Models/TeacherBusinessModelAdmin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace business.Models
+{
+    public class TeacherBusinessModelAdmin
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string SName { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
