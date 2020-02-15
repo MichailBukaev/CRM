6640b771272489f473be20fcce651ae47bb00d2a
filin0wl <nastia-dogf@mail.ru>
2020-02-15 18:03:14 +0300

    Add new models LinkTeacherCourse

---
+++ data/StorageEntity/StorageLinkTeacherCourse.cs
@@ -0,0 +1,80 @@
+﻿using data.Storage;
+using models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace data.StorageEntity
+{
+    class StorageLinkTeacherCourse : IStorage
+    {
+        IReader _reader;
+
+        public StorageLinkTeacherCourse()
+        {
+            _reader = new ReaderLinkTeacherCourse();
+        }
+        public bool Add(IEntity obj)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                if (obj != null)
+                {
+                    db.LinkTeacherCourses.Add((LinkTeacherCourse)obj);
+                    db.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public bool Delete(IEntity obj)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                if (obj != null)
+                {
+                    db.LinkTeacherCourses.Remove((LinkTeacherCourse)obj);
+                    db.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public IEnumerable<IEntity> GetAll(string Tkey, string TValue)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var links = db.LinkTeacherCourses.ToList();
+                return _reader.Read(Tkey, TValue, links);
+
+            }
+        }
+
+        public IEnumerable<IEntity> GetAll()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var links = db.LinkTeacherCourses.ToList();
+                return _reader.Read(null, null, links);
+
+            }
+        }
+
+        public bool Update(IEntity obj)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                if (obj != null)
+                {
+                    db.LinkTeacherCourses.Update((LinkTeacherCourse)obj);
+                    db.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
