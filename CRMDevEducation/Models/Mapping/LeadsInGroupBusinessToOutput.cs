ca1cdf1135fba1fda41d0ce1e3eb506a90627226
vitaminka <veta-sid@mail.ru>
2020-02-15 16:46:17 +0300

    input, output, business models for Log and Group

---
+++ CRMDevEducation/Models/Mapping/LeadsInGroupBusinessToOutput.cs
@@ -0,0 +1,27 @@
+﻿using CRMDevEducation.Models.Output;
+using business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMDevEducation.Models.Mapping
+{
+    public class LeadsInGroupBusinessToOutput
+    {
+        public static List<OutputLeadsInGroupModel> Map(List<LeadsInGroupBusinessModel> models)
+        {
+            List<OutputLeadsInGroupModel> output = new List<OutputLeadsInGroupModel>();
+            foreach (LeadsInGroupBusinessModel item in models)
+	        {
+                output.Add(new OutputLeadsInGroupModel()
+                {
+                    Id = item.Id,
+                    FName = item.FName,
+                    SName = item.SName
+                });
+	        }
+            return output;            
+        }
+    }
+}
---
+++ CRMDevEducation/Models/Mapping/LogMappingInputToBusiness.cs
@@ -0,0 +1,21 @@
+﻿using business.Models;
+using CRMDevEducation.Models.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMDevEducation.Models.Mapping
+{
+    public class LogMappingInputToBusiness
+    {
+        public static LogBusinessModel Map(InputLogModel model)
+        {
+            return new LogBusinessModel()
+            {
+                Date = model.Date,
+                LeadsVisit = model.LeadsVisit,               
+            };
+        }
+    }
+}
