4b342d37ffd38d5014fa0f348af371dbe4250688
vitaminka <veta-sid@mail.ru>
2020-02-17 11:50:46 +0300

    DeleteGroup, CreateGroup

--- CRMDevEducation/Controllers/HomeHeadHRController.cs
+++ CRMDevEducation/Controllers/HomeHeadHRController.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using business;
 using business.WSHR;
+using CRMDevEducation.Models.Input;
+using CRMDevEducation.Models.Mapping;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace CRMDevEducation.Controllers
 {
@@ -22,5 +26,46 @@ namespace CRMDevEducation.Controllers
             manager = new HeadHR(hr);
         }
 
+        [Route("CreateGroup")]
+        [HttpPost]
+        public string CreateGroup(InputGroupModel model)
+        {
+            if (StorageToken.Check(Request.Headers["Authorization"]))
+            {
+                if (manager.CreateGroup(GroupMappingInputToBusiness.Map(model)))
+                {
+                    return "true";
+                }
+                else
+                {
+                    return "false";
+                }
+            }
+            else
+            {
+                return "Bad Login";
+            }
+        }
+
+        [Route("DeleteGroup")]
+        [HttpPost]
+        public string DeleteGroup(InputGroupModel model)
+        {
+            if (StorageToken.Check(Request.Headers["Authorization"]))
+            {
+                if (manager.DeleteGroup(GroupMappingInputToBusiness.Map(model)))
+                {
+                    return "true";
+                }
+                else
+                {
+                    return "false";
+                }
+            }
+            else
+            {
+                return "Bad Login";
+            }
+        }
     }
 }
