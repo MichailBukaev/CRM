a515c5d3bf31024b36018230f4b28a2af21ca66e
vitaminka <veta-sid@mail.ru>
2020-02-19 15:13:10 +0300

    cache

---
+++ business/Cache/CacheSkills.cs
@@ -0,0 +1,13 @@
+﻿using business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace business.Cache
+{
+    class CacheSkills
+    {
+        public bool FlagActual { get; set; }
+        public List<LeadBusinessModel> Leads;
+    }
+}
