6d776974e416f0cfa9aae2fb12703221428eb730
Dimonchan <dt97@mail.ru>
2020-02-19 15:31:01 +0300

    Add Cache for Course,Group,Status

---
+++ business/Cache/CacheCourse.cs
@@ -0,0 +1,23 @@
+﻿using business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace business.Cache
+{
+    public class CacheCourse
+    {
+        public List<CourseBusinessModel> Courses { get; set; }
+        public bool FlagActual { get; set; }
+        public CacheCourse()
+        {
+            FlagActual = false;
+        }
+        public void ReadChange()
+        {
+            FlagActual = false;
+        }
+
+
+    }
+}
---
+++ business/Cache/CacheGroup.cs
@@ -0,0 +1,20 @@
+﻿using business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace business.Cache
+{
+    public class CacheGroup
+    {
+        public List <GroupBusinessModel> Groups { get; set; }
+        public bool FlagActual { get; set; }
+        public CacheGroup() {
+            FlagActual = false;
+        }
+        public void ReadChange()
+        {
+            FlagActual = false;
+        }
+    }
+}
