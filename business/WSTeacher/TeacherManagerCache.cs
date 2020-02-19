1432b0001685a41b0f9defa5cc6034377645573c
Dimonchan <dt97@mail.ru>
2020-02-19 15:47:47 +0300

    Added TeacherManagerCache

---
+++ business/WSTeacher/TeacherManagerCache.cs
@@ -0,0 +1,18 @@
+﻿using business.Cache;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace business.WSTeacher
+{
+    public class TeacherManagerCache
+    {
+        public CacheTeachers Teachers { get; set; }
+        public List <CacheLeads> Leads { get; set; }
+        public CacheGroup Group { get; set; }
+        public CacheCourse Course { get; set; }
+        public CacheSkills Skills { get; set; }
+        public CacheStatus Status { get; set; }
+        
+    }
+}
