521e92a1647bcf5253dd822479ef95d968d07907
filin0wl <nastia-dogf@mail.ru>
2020-02-19 18:43:48 +0300

    Начата организация цепочки обязаностей

--- business/Models/GroupBusinessModel.cs
+++ business/Models/GroupBusinessModel.cs
@@ -9,7 +9,7 @@ namespace business.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int CourseId { get; set; }        
-        public int TeacherId { get; set; }
+        public int? TeacherId { get; set; }
         public List<int> LeadsId { get; set; }
         public string StartDate { get; set; }
         public LogBusinessModel LogOfGroup { get; set; }
---
+++ business/WSTeacher/HandlerTeacher.cs
@@ -0,0 +1,19 @@
+﻿using data.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace business.WSTeacher
+{
+    public abstract class HandlerTeacher
+    {
+
+       protected IStorage storage;
+        public HandlerTeacher Next { get; set; }
+
+        public abstract void FillUp(TeacherManagerCache teacherManagerCache, int? id);
+
+
+
+    }
+}
