2a52a3e91c4b8d68f73987dd8b99444329cf2ba2
Violetta <veta-sid@mail.ru>
2020-02-25 12:39:11 +0300

    Create GroupController.cs

---
+++ CRMDevEducation/Controllers/GroupController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRMDevEducation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupController : ControllerBase
+    {
+        //показать журнал teacher, hr
+        //показать группу teacher, hr
+        //добавить комментарий группе ?
+        //добавить дату в журнал teacher
+    }
+}
