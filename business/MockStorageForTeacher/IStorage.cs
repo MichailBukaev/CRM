65fd57aab87dc0591e31b6d1d872d28270ccdec4
filin0wl <nastia-dogf@mail.ru>
2020-02-28 00:06:53 +0300

    tests

---
+++ business/MockStorageForTeacher/IStorage.cs
@@ -0,0 +1,16 @@
+﻿using models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace business.MockStorageForTeacher
+{
+    public interface IStorage
+    {
+        string GetAll();
+        string GetAll(string Tkey, string TValue);
+        string Update(IEntity obj);
+        string Add(ref IEntity obj);
+        string Delete(IEntity obj);
+    }
+}
---
+++ business/MockStorageForTeacher/StorageCourse.cs
@@ -0,0 +1,38 @@
+﻿using models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace business.MockStorageForTeacher
+{
+    public class StorageCourse : IStorage
+    {
+        public string Add(ref IEntity obj)
+        {
+            if (obj is Course)
+                return "StorageCourse.Add()";
+            else
+                return "StorageCourse error type obj";
+        }
+
+        public string Delete(IEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAll(string Tkey, string TValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(IEntity obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
