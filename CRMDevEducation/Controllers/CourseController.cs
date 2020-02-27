dd58a5d146500dfe17b509932c81e8e1052e7b31
vitaminka <veta-sid@mail.ru>
2020-02-27 22:33:05 +0300

    CourseController

--- CRMDevEducation/Controllers/CourseController.cs
+++ CRMDevEducation/Controllers/CourseController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using business;
 using business.Models;
+using business.WSTeacher.HeadTeacher;
 using business.WSUser.interfaces;
+using CRMDevEducation.Models.Input;
 using CRMDevEducation.Models.Mapping;
 using CRMDevEducation.Models.Output;
 using Microsoft.AspNetCore.Authorization;
@@ -32,6 +36,23 @@ namespace CRMDevEducation.Controllers
                 json += JsonSerializer.Serialize<OutputCourseModel>(CourseMappingBusinessToOutpot.Map((CourseBusinessModel)_manager.GetCourse(id)));
             }
             return json;
-        }      
+        }
+
+        [Authorize(Roles = "HeadTeacher")]
+        [HttpPost]
+        public HttpResponseMessage PostTeacherToCourse(int _courseId, int _teacherId)
+        {
+            MaxHeadTeacherManager _manager = (MaxHeadTeacherManager)StorageToken.GetManager(Request.Headers["Authorization"]);
+            
+            if (_manager != null && StorageToken.Check(Request.Headers["Authorization"]))
+            {
+                bool result = _manager.AssignTeacherForCourse(_teacherId, _courseId);
+                if(result)
+                    return new HttpResponseMessage(HttpStatusCode.OK);
+                else return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+            else 
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+        }
     }
 }
